679c01884dbb25b2d16fc2d1785c30d9076ecd5e
Ryo Akagi <ryoakg@gmail.com>
2016-07-18 15:31:25 +0900

    removes unused functions in cache.rs.
    download, decompress, update and clear

--- src/cache.rs
+++ src/cache.rs
@@ -1,4 +1,3 @@
-use std::io::Read;
 use std::fs;
 use std::env;
 use std::path::PathBuf;
@@ -6,13 +5,10 @@ use std::path::PathBuf;
 #[cfg(unix)] use std::os::unix::fs::MetadataExt;
 
 use xdg::BaseDirectories;
-use flate2::read::GzDecoder;
-use tar::Archive;
-use curl::http;
 use walkdir::{WalkDir, WalkDirIterator, DirEntry};
 use time;
 
-use error::TealdeerError::{self, CacheError, UpdateError};
+use error::TealdeerError::{self, CacheError};
 use types::OsType;
 
 #[derive(Debug)]
@@ -52,56 +48,6 @@ impl Cache {
             Err(_) => return Err(CacheError("Could not determine XDG base directory.".into())),
         };
         Ok(xdg_dirs.get_cache_home())
-    }
-
-    /// Download the archive
-    fn download(&self) -> Result<http::Response, TealdeerError> {
-        let resp = try!(
-            http::handle()
-                 .follow_location(1)
-                 .get(&self.url[..])
-                 .exec()
-        );
-        Ok(resp)
-    }
-
-    /// Decompress and open the archive
-    fn decompress<R: Read>(&self, reader: R) -> Result<Archive<GzDecoder<R>>, TealdeerError> {
-        let decoder = try!(GzDecoder::new(reader).map_err(|_| UpdateError("Could not decode gzip data".into())));
-        Ok(Archive::new(decoder))
-    }
-
-    /// Update the pages cache.
-    pub fn update(&self) -> Result<(), TealdeerError> {
-        // First, download the compressed data
-        let response = try!(self.download());
-
-        // Decompress the response body into an `Archive`
-        let mut archive = try!(self.decompress(response.get_body()));
-
-        // Determine paths
-        let cache_dir = try!(self.get_cache_dir());
-
-        // Make sure that cache directory exists
-        debug!("Ensure cache directory {:?} exists", &cache_dir);
-        try!(fs::create_dir_all(&cache_dir).map_err(|e| {
-            UpdateError(format!("Could not create cache directory: {}", e))
-        }));
-
-        // Clear cache directory
-        // Note: This is not the best solution. Ideally we would download the
-        // archive to a temporary directory and then swap the two directories.
-        // But renaming a directory doesn't work across filesystems and Rust
-        // does not yet offer a recursive directory copying function. So for
-        // now, we'll use this approach.
-        try!(self.clear());
-
-        // Extract archive
-        try!(archive.unpack(&cache_dir).map_err(|e| {
-            UpdateError(format!("Could not unpack compressed data: {}", e))
-        }));
-
-        Ok(())
     }
 
     #[cfg(unix)]
@@ -208,20 +154,5 @@ impl Cache {
         pages.sort();
         pages.dedup();
         Ok(pages)
-    }
-
-    /// Delete the cache directory.
-    pub fn clear(&self) -> Result<(), TealdeerError> {
-        let path = try!(self.get_cache_dir());
-        if path.exists() && path.is_dir() {
-            try!(fs::remove_dir_all(&path).map_err(|_| {
-                CacheError(format!("Could not remove cache directory ({}).", path.display()))
-            }));
-        } else if path.exists() {
-            return Err(CacheError(format!("Cache path ({}) is not a directory.", path.display())));
-        } else {
-            return Err(CacheError(format!("Cache path ({}) does not exist.", path.display())));
-        };
-        Ok(())
     }
 }
