3de8cfde48dbef5fe18c692446c4c68ecb11ed94
Ryo Akagi <ryoakg@gmail.com>
2016-07-18 18:34:47 +0900

    modify formatter

--- src/formatter.rs
+++ src/formatter.rs
@@ -26,11 +26,11 @@ fn format_braces(text: &str) -> String {
 pub fn print_lines<R>(tokenizer: &mut Tokenizer<R>) where R: BufRead {
     while let Some(token) = tokenizer.next_token() {
         match token {
-            LineType::Empty => println!(""),
+            LineType::Empty => print!(""),
             LineType::Title(_) => debug!("Ignoring title"),
-            LineType::Description(text) => println!("  {}", text),
+            LineType::Description(text) => println!("  {}\n", text),
             LineType::ExampleText(text) => println!("  {}", Colour::Green.paint(format!("- {}", text))),
-            LineType::ExampleCode(text) => println!("  {}", &format_braces(&text)),
+            LineType::ExampleCode(text) => println!("    {}", &format_braces(&text)),
             LineType::Other(text) => debug!("Unknown line type: {:?}", text),
         }
     }
